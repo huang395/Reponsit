d3a713502da1c1f95d8b8aa521f2fc7cfc04c26f
huang395 <102885623+huang395@users.noreply.github.com>
2022-04-08 21:56:02 +0800

    Update and rename snake game to snake game.c

--- snake game.c
+++ snake game.c
@@ -332,6 +332,3 @@ void start_game()
   } 
  }
 }
-————————————————
-版权声明：本文为CSDN博主「embed_huang」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
-原文链接：https://blog.csdn.net/huangblog/article/details/8294326
